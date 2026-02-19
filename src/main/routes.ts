fb3a083fb8238cad47f505333fde361834a3e0f0
Elizabethyonas <elizabethyonas700@gmail.com>
2026-02-19 13:50:43 +0300

    init

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import adminAuthRoutes from '../modules/admin/auth/routes/auth.routes';
 import driverAuthRoutes from '../modules/driver/auth/routes/auth.routes';
+import driverProfileRoutes from '../modules/driver/profile/routes/profile.routes';
 import garageAuthRoutes from '../modules/garage/auth/routes/auth.routes';
 
 const rootRouter = Router();
 
 rootRouter.use('/admin/auth', adminAuthRoutes);
 rootRouter.use('/drivers/auth', driverAuthRoutes);
+rootRouter.use('/driver', driverProfileRoutes);
 rootRouter.use('/garages/auth', garageAuthRoutes); 
 
 
